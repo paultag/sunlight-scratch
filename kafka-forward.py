ab360d6eed15a9be395d7b61c27225fc1ded85c8
Paul Tagliamonte <tag@pault.ag>
2015-05-04 17:02:18 -0400

    fiddle about with sending structure.

--- kafka-forward.py
+++ kafka-forward.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import os
+import uuid
 import json
 import threading, logging, time
 from kafka.client import KafkaClient
@@ -7,9 +9,10 @@ from kafka.producer import SimpleProducer
 
 import requests
 
-# from opencivicdata.api.client import VagrantOCDAPI
-# api = VagrantOCDAPI()
-# XXX Fix the above.
+from opencivicdata.api.client import VagrantOCDAPI
+api = VagrantOCDAPI()
+
+DATA_ROOT = os.path.abspath("./data")
 
 
 def iternodes(message):
@@ -24,33 +27,34 @@ class OCDForwarder(object):
     # post-scrape-reports
     # incoming-data
 
-    def run(self):
+    def run(self, quick=None):
+        self.quick = False if quick is None else True
+
         client = KafkaClient("localhost:9092")
+        self.producer = SimpleProducer(client)
         self.consumer = SimpleConsumer(
             client,
-            b"post-scrape-forwss",
+            str(uuid.uuid4()).encode(),
             b"post-scrape-reports",
             max_buffer_size=None,
         )
 
         for message in self.consumer:
             message = json.loads(message.message.value.decode('utf-8'))
-            self.producer = SimpleProducer(client, batch_send=True)
+            for _, id_ in iternodes(message):
+                self.fetch_data(id_)
+
             for action, id_ in iternodes(message):
                 self.send_data(action, id_)
-            self.producer.stop()
 
     def send_data(self, action, id_):
-        url = "http://10.42.2.102/{}".format(id_)
-        print(url)
-        print(url)
-        print(url)
-        print(url)
-        print(url)
-        response = requests.get(url)
-        data = response.json()  # XXX: Fixme
-        if response.status_code != 200:
-            raise ValueError("Bailed: {}".format(data.text))
+        klass, _ = id_.split("/", 1)
+        if klass in ['ocd-membership', 'ocd-post']:
+            return
+
+        path = os.path.join(DATA_ROOT, id_)
+        with open(path, 'r') as fd:
+            data = json.load(fd)
 
         self.producer.send_messages('incoming-data', json.dumps({
             "action": action,
@@ -58,9 +62,38 @@ class OCDForwarder(object):
         }).encode())
 
 
+    def fetch_data(self, id_, fail=0):
+        klass, _ = id_.split("/", 1)
+        if klass in ['ocd-membership', 'ocd-post']:
+            """
+            TODO: What to do about the above
+            """
+            return
+
+        path = os.path.join(DATA_ROOT, id_)
+        root = os.path.dirname(path)
+
+        if self.quick and os.path.exists(path):
+            return
+
+        try:
+            data = api._get_object(id_)
+        except ValueError:
+            if fail > 3:
+                raise
+            return self.fetch_data(id_, fail=(fail + 1))
+
+        if not os.path.exists(root):
+            os.makedirs(root)
+
+        with open(path, 'w') as fd:
+            json.dump(data, fd)
+
+
 if __name__ == "__main__":
+    import sys
     logging.basicConfig(
         format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s',
         level=logging.DEBUG
         )
-    OCDForwarder().run()
+    OCDForwarder().run(*sys.argv[1:])
